0e4f7301296b4df0521bff3b38fed15c7a238667
Michał Kosowski <kosowski94@gmail.com>
2018-06-21 08:53:55 +0200

    Add generator

--- euler-path/js/game.js
+++ euler-path/js/game.js
@@ -62,6 +62,119 @@ var createShape = function(game) {
 	});
 };
 
+function distance(obj1, obj2){
+	return Math.sqrt(Math.pow(obj1[0] - obj2[0], 2) + Math.pow(obj1[1] - obj2[1], 2));
+}
+
+function genGraph(){
+	var numberOfNodes = getNumberOfNodes();
+	var nodes = genNodes(numberOfNodes);
+	var edges = [];
+	var currentNode = Math.floor(Math.random() * nodes.length);
+	var nodesNotUsed = [];
+	for(var i = 0; i < nodes.length; i++){
+		if(currentNode != i){
+			nodesNotUsed.push(i);
+		}
+	}
+
+	for(var i = 0; i < getNumberOfEdges(numberOfNodes); i++){
+		var newNode = Math.floor(Math.random() * nodes.length);
+		if(currentNode != newNode){
+			var add = true;
+			for(var j = 0; j < edges.length; j++){
+				if((edges[j][0] == currentNode && edges[j][1] == newNode) || (edges[j][1] == currentNode && edges[j][0] == newNode)){
+					add = false;
+					i--;
+					break;
+				}
+			}
+			if(!add){
+				continue;
+			}
+			edges.push([currentNode, newNode]);
+			currentNode = newNode;
+			if(nodesNotUsed.indexOf(newNode) > -1){
+				nodesNotUsed.splice(nodesNotUsed.indexOf(newNode), 1);
+			}
+		}
+	}
+
+	for(var i = 0; i < nodesNotUsed.length; i++){
+		edges.push([currentNode, nodesNotUsed[i]]);
+		currentNode = nodesNotUsed[i];
+	}
+
+	console.log("not used");
+	console.log(nodesNotUsed);
+	console.log(edges);
+	var graph = {"nodesData": nodes, "edgesData": []};
+	for(var i = 0; i < edges.length; i++){
+		graph.edgesData.push([nodes[edges[i][0]][0], nodes[edges[i][0]][1], nodes[edges[i][1]][0], nodes[edges[i][1]][1]]);
+	}
+	return [graph];
+}
+
+function getNumberOfNodes(){
+	var age = config.age;
+	if(age < 7){
+		return random(3, 5);
+	}
+	if(age < 11){
+		return random(4, 6);
+	}
+	if(age < 15){
+		return random(5, 7);
+	}
+	if(age < 19){
+		return random(6, 8);
+	}
+	return random(7, 9);
+}
+
+function getNumberOfEdges(numberOfNodes){
+	var age = config.age;
+	if(age < 7){
+		return Math.floor(numberOfNodes * 1.5);
+	}
+	if(age < 11){
+		return Math.floor(numberOfNodes * 1.75);
+	}
+	if(age < 15){
+		return Math.floor(numberOfNodes * 2);
+	}
+	if(age < 19){
+		return Math.floor(numberOfNodes * 2.25);
+	}
+	return Math.floor(numberOfNodes * 2.5);
+}
+
+function random(a, b){
+	return Math.floor(Math.random() * (b - a + 1) + a);
+}
+
+function genNodes(numberOfNodes){
+	var width = 800;
+	var height = 1000;
+	var radius = 40;
+	var nodes = [];
+	for(var i = 0; i < numberOfNodes; i++){
+		var nodeAdded = false;
+		while(!nodeAdded){
+			var newNode = [Math.floor(Math.random() * (width - 2*radius) + radius), Math.floor(Math.random() * (height - 2*radius) + radius)];
+			nodeAdded = true;
+			for(var j = 0; j < nodes.length; j++){
+				if(distance(newNode, nodes[j]) < 6 * radius){
+					nodeAdded = false;
+					break;
+				}
+			}
+		}
+		nodes.push(newNode);
+	}
+	return nodes;
+}
+
 var Game = function(shapesData) {
 	// var shapesData = [];
 	console.log(config);
@@ -78,6 +191,10 @@ var Game = function(shapesData) {
 		shape = x[4];
 	}
 	
+	// shape = [{"nodesData": genNodes(), "edgesData": []}]
+	shape = genGraph();
+	console.log(shape);
+
 	this.shapesData = shapesData || shape; // || [shapeData1, shapeData2, shapeData3];
 	this.shapesData = [this.shapesData[Math.floor(Math.random() * this.shapesData.length)]]
 	this.noOfLevels = this.shapesData.length;
